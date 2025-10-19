fb57958ceff09595b218249055cae7c6d0c56de4
luismetal-web <luisfergarcia08021@gmail.com>
2025-10-19 18:32:26 +0200

    Add application file

--- app.py
+++ app.py
@@ -1,2 +1,9 @@
-import streamlit as st
-st.write("hello world")
+import gradio as gr
+from transformers import pipeline
+
+generator = pipeline("text-generation", model="TheBloke/vicuna-7B-1.1-HF", use_auth_token="TU_TOKEN_HF")
+
+def chat(input_text):
+    return generator(input_text, max_length=200)[0]['generated_text']
+
+gr.Interface(fn=chat, inputs="text", outputs="text").launch()
